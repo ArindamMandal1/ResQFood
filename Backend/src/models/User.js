879f8014be083f1762e4f1cb3ef0b03f6ee0e9a9
Sreejib-Nandy <sreejibnandy2518@gmail.com>
2025-11-15 21:12:49 +0530

    Fix start script for render

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name : {type : String, required : true},
-    email : {type : String, unique: true, required : true, lowercase : true,},
+    email : {type : String, unique: true, required : true, lowercase : true},
     password : {type : String, required : true},
     role : { type : String, enum : ["restaurant", "ngo"], required : true },
     address : { type: String, required : true},
